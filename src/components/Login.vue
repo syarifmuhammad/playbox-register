d9381c5f982727511e61f6ae0412aa9352cd105d
abadan03 <dinarabadan03@gmail.com>
2022-04-25 22:02:35 +0700

    save

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,37 @@
 <template>
-    <main class="container" id="main-content" style="height:100%;">
-        <div style="height:100%;" class="row d-flex justify-content-center align-items-center">
-            <div id="login" class="col-xs-11 col-sm-10 col-md-6 col-xl-4 d-flex flex-column align-items-center justify-content-center py-3 px-4">
-                <img src="images/logo-playbox-s2-fix.png" class="img-fluid w-50" alt="">
-                <form class="form d-flex flex-column w-100">
-                    <input class="form-control my-3" type="email" placeholder="Email" required>
-                    <input class="form-control" type="password" placeholder="Password" required>
-                    <button class="btn btn-outline-primary my-3">Login</button>
-                    <span>Belum punya akun? <a @click="">Daftar</a></span>
-                </form>
-                
-            </div>
-        </div>
-    </main>
+  <main class="container" id="main-content" style="height: 100%">
+    <div
+      style="height: 100%"
+      class="row d-flex justify-content-center align-items-center"
+    >
+      <div
+        id="login"
+        class="col-xs-11 col-sm-10 col-md-6 col-xl-4 d-flex flex-column align-items-center justify-content-center py-3 px-4"
+      >
+        <img
+          src="images/logo-playbox-s2-fix.png"
+          class="img-fluid w-50"
+          alt=""
+        />
+        <form class="form d-flex flex-column w-100">
+          <input
+            class="form-control my-3"
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            class="form-control"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <button class="btn btn-outline-primary my-3">Login</button>
+          <span>Belum punya akun? <a>Daftar</a></span>
+        </form>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,16 +1,40 @@
 <template>
-    <main class="container" id="main-content" style="height:100%;">
-        <div style="height:100%;" class="row d-flex justify-content-center align-items-center">
-            <div id="register" class="col-xs-11 col-sm-10 col-md-6 col-xl-4 d-flex flex-column align-items-center justify-content-center py-3 px-4">
-                <img src="assets/logo-playbox-s2-fix.png" class="img-fluid w-50" alt="">
-                <form class="form d-flex flex-column w-100">
-                    <input class="form-control my-3" type="email" placeholder="Email" required>
-                    <input class="form-control" type="tel" placeholder="No Telepon" required>
-                    <input class="form-control my-3" type="password" placeholder="Password" required>
-                    <button class="btn btn-outline-primary my-3">Daftar</button>
-                </form>
-                
-            </div>
-        </div>
-    </main>
+  <main class="container" id="main-content" style="height: 100%">
+    <div
+      style="height: 100%"
+      class="row d-flex justify-content-center align-items-center"
+    >
+      <div
+        id="register"
+        class="col-xs-11 col-sm-10 col-md-6 col-xl-4 d-flex flex-column align-items-center justify-content-center py-3 px-4"
+      >
+        <img
+          src="assets/logo-playbox-s2-fix.png"
+          class="img-fluid w-50"
+          alt=""
+        />
+        <form class="form d-flex flex-column w-100">
+          <input
+            class="form-control my-3"
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            class="form-control"
+            type="tel"
+            placeholder="No Telepon"
+            required
+          />
+          <input
+            class="form-control my-3"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <button class="btn btn-outline-primary my-3">Daftar</button>
+        </form>
+      </div>
+    </div>
+  </main>
 </template>
