9d5da3ed0d1c07a16c8f28be89c85d1554e192dc
abadan03 <dinarabadan03@gmail.com>
2022-05-06 21:01:04 +0700

    add ide page with css | add ide.vue

---
+++ src/components/ide.vue
@@ -0,0 +1,33 @@
+<template>
+<body>
+    <div class="card mt-3">
+      <div class="card-body">
+        <h4 class="text-center fs-4 mb-3 fw-bold">IDE TIM</h4>
+        <div class="d-flex justify-content-center row"></div>
+        <div class="row pb-3">
+          <div class="col-sm-12 col-md-6 col-lg-4 mt-4 center">
+            <span class="text-warning fw-bold"></span>
+
+            <div class="card p-3">
+              <div class="form-outline">
+                <span class="label label-primary">Judul</span>
+
+                <input type="text" class="form-control center" id="" />
+              </div>
+              <form action="" class="form">
+                <div class="form-outline">
+                  <label for="comment">Deskripsi</label>
+                  <input type="text" class="form-control center" id="" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-end" style="margin-top: 20px">
+          <button class="w-30 btn btn-primary fw-bold disabled">
+            SIMPAN & LANJUT
+          </button>
+        </div>
+      </div>
+    </div>
+</template>
